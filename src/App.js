7c8f018291964c9228e2f4048c26e39aba9426f0
MurthyAvanithsa <murthyavanithsa@gmail.com>
2021-05-17 16:12:04 +0530

    Adding base layout

--- src/App.js
+++ src/App.js
@@ -6,22 +6,53 @@ import {
   Link,
   LinkDesign,
   ShellBar,
-  ThemeProvider
-} from '@ui5/webcomponents-react';
-import React from 'react';
-import './App.css';
+  SideNavigation,
+  SideNavigationItem,
+  SideNavigationSubItem,
+  ThemeProvider,
+} from "@ui5/webcomponents-react";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <ThemeProvider>
       <ShellBar primaryTitle="UI5 Web Components for React Template" />
       <FlexBox
-        style={{ width: '100%', height: '100vh' }}
+        style={{ width: "100%", height: "100vh" }}
         direction={FlexBoxDirection.Column}
         justifyContent={FlexBoxJustifyContent.Center}
         alignItems={FlexBoxAlignItems.Center}
       >
-        <Link href="https://sap.github.io/ui5-webcomponents-react/" target="_blank" design={LinkDesign.Emphasized}>
+        <SideNavigation
+          className=""
+          fixedItems={
+            <>
+              <SideNavigationItem icon="chain-link" text="Useful Links" />
+              <SideNavigationItem icon="history" text="History" />
+            </>
+          }
+          onSelectionChange={function noRefCheck() {}}
+          slot=""
+          style={{}}
+          tooltip=""
+        >
+          <SideNavigationItem icon="home" text="Home" />
+          <SideNavigationItem expanded icon="group" text="People">
+            <SideNavigationSubItem text="From My Team" />
+            <SideNavigationSubItem text="From Other Teams" />
+          </SideNavigationItem>
+          <SideNavigationItem icon="locate-me" selected text="Locations" />
+          <SideNavigationItem icon="calendar" text="Events">
+            <SideNavigationSubItem text="Local" />
+            <SideNavigationSubItem text="Others" />
+          </SideNavigationItem>
+        </SideNavigation>
+        <Link
+          href="https://sap.github.io/ui5-webcomponents-react/"
+          target="_blank"
+          design={LinkDesign.Emphasized}
+        >
           Getting Started with UI5 Web Component for React
         </Link>
       </FlexBox>
