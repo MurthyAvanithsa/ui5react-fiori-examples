d6ea274992d135377f1488e5f6938f4cdafde67a
MurthyAvanithsa <murthyavanithsa@gmail.com>
2021-05-17 17:12:28 +0530

    Adding dyanmiac page

--- src/App.js
+++ src/App.js
@@ -10,21 +10,51 @@ import {
   SideNavigationItem,
   SideNavigationSubItem,
   ThemeProvider,
+  DynamicPage,
+  Label,
+  DynamicPageHeader,
+  ObjectStatus,
+  DynamicPageTitle,
+  Button,
+  Breadcrumbs,
+  Badge,
+  Title,
+  Form,
+  FormItem,
+  Input,
+  FormGroup,
+  Select,
+  Option,
+  TextArea,
+  CheckBox,
 } from "@ui5/webcomponents-react";
 import React from "react";
 import "./App.css";
 
+import { useState } from "react";
+
 function App() {
+  const [expand, setExpand] = useState(true);
+  const handleClick = (event) => {
+    setExpand((prevExpand) => !prevExpand);
+  };
   return (
     <ThemeProvider>
-      <ShellBar primaryTitle="UI5 Web Components for React Template" />
+      <ShellBar primaryTitle="UI5 For PRA">
+        <ui5-button
+          icon="menu"
+          slot="startButton"
+          id="startButton"
+          onClick={handleClick}
+        ></ui5-button>
+      </ShellBar>
       <FlexBox
         style={{ width: "100%", height: "100vh" }}
-        direction={FlexBoxDirection.Column}
-        justifyContent={FlexBoxJustifyContent.Center}
-        alignItems={FlexBoxAlignItems.Center}
+        // direction={FlexBoxDirection.Column}
+        // justifyContent={FlexBoxJustifyContent.Center}
       >
         <SideNavigation
+          collapsed={expand}
           className=""
           fixedItems={
             <>
@@ -48,13 +78,143 @@ function App() {
             <SideNavigationSubItem text="Others" />
           </SideNavigationItem>
         </SideNavigation>
-        <Link
-          href="https://sap.github.io/ui5-webcomponents-react/"
-          target="_blank"
-          design={LinkDesign.Emphasized}
+        <DynamicPage
+          header={
+            <DynamicPageHeader>
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <Label>Location: Warehouse A</Label>
+                <Label>Halway: 23L</Label>
+                <Label>Rack: 34</Label>
+                <FormItem label="Sole Form Item">
+                  <Input />
+                </FormItem>
+
+                <FormItem label="Sole Form Item">
+                  <Input />
+                </FormItem>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginLeft: "2vw",
+                }}
+              >
+                <Label>Availability:</Label>
+                <ObjectStatus state="Success">In Stock</ObjectStatus>
+              </div>
+            </DynamicPageHeader>
+          }
+          style={{
+            maxHeight: "700px",
+          }}
+          title={
+            <DynamicPageTitle
+              actions={[
+                <Button key="edit" design="Emphasized">
+                  Edit
+                </Button>,
+                <Button key="delete" design="Transparent">
+                  Delete
+                </Button>,
+                <Button key="copy" design="Transparent">
+                  Copy
+                </Button>,
+                <Button key="action" design="Transparent" icon="action" />,
+              ]}
+              breadcrumbs={
+                <Breadcrumbs>
+                  <Link>Home</Link>
+                  <Link>Page 1</Link>
+                  <Link>Page 2</Link>
+                  <Link>Page 3</Link>
+                  <Link>Page 4</Link>
+                  <Link>Page 5</Link>
+                </Breadcrumbs>
+              }
+              heading={<Title>Header Title</Title>}
+              navigationActions={[
+                <Button
+                  key="fullscreen"
+                  design="Transparent"
+                  icon="full-screen"
+                />,
+                <Button
+                  key="exitFullscreen"
+                  design="Transparent"
+                  icon="exit-full-screen"
+                />,
+                <Button key="decline" design="Transparent" icon="decline" />,
+              ]}
+              subHeading={<Label>This is a subheading</Label>}
+            >
+              <Badge>Status: OK</Badge>
+            </DynamicPageTitle>
+          }
         >
-          Getting Started with UI5 Web Component for React
-        </Link>
+          <Form title="Test Form">
+            <FormItem label="Sole Form Item">
+              <Input />
+            </FormItem>
+            <FormGroup title="Personal Data">
+              <FormItem label="Name">
+                <Input />
+              </FormItem>
+              <FormItem label={<Label>Address</Label>}>
+                <Input />
+              </FormItem>
+              <FormItem label="Country">
+                <Select>
+                  <Option>Germany</Option>
+                  <Option>France</Option>
+                  <Option>Italy</Option>
+                </Select>
+              </FormItem>
+              <FormItem label="Additional Comment">
+                <TextArea
+                  rows={5}
+                  style={{
+                    width: "210px",
+                  }}
+                />
+              </FormItem>
+              <FormItem label="Home address">
+                <CheckBox checked />
+              </FormItem>
+            </FormGroup>
+            <FormGroup title="Company Data">
+              <FormItem label="Company Name">
+                <Input />
+              </FormItem>
+              <FormItem label="Company Address">
+                <Input />
+              </FormItem>
+              <FormItem label="Company City">
+                <Input />
+              </FormItem>
+              <FormItem label="Company Country">
+                <Input />
+              </FormItem>
+              <FormItem label="Number of Employees">
+                <Input disabled type="Number" value="5000" />
+              </FormItem>
+              <FormItem label="Member of Partner Network">
+                <CheckBox checked />
+              </FormItem>
+            </FormGroup>
+            <FormGroup title="Marketing Data">
+              <FormItem label="Email">
+                <Input type="Email" />
+              </FormItem>
+              <FormItem label="Company Email">
+                <Input type="Email" />
+              </FormItem>
+              <FormItem label="I want to receive the newsletter">
+                <CheckBox />
+              </FormItem>
+            </FormGroup>
+          </Form>
+        </DynamicPage>
       </FlexBox>
     </ThemeProvider>
   );
