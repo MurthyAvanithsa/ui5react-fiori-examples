0975827ab31dea09d4b567d83bc9dde8b6ed8ebd
Niyati25 <niyatip25@gmail.com>
2021-05-24 23:24:53 +0530

    1.  enabled scrolling even on the filter box 2. fixed issue of the table height

--- src/Pages/WellCompletion/index.js
+++ src/Pages/WellCompletion/index.js
@@ -119,6 +119,7 @@ export const WellCompletion = () => {
           </Form> */}
 
           <FilterBar
+            style={{ maxHeight: "500px" }}
             showGoOnFB
             filterContainerWidth="13.125rem"
             search={<Input placeholder="Search" />}
@@ -207,7 +208,7 @@ export const WellCompletion = () => {
         </DynamicPageHeader>
       }
       style={{
-        maxHeight: "700px",
+        maxHeight: "calc(100vh-44px)",
       }}
     >
       <WellCompletionTable />
