5521c07a138eef1dbf76cc095dbddba9c8ef57b3
Niyati25 <niyatip25@gmail.com>
2021-05-25 14:41:31 +0530

    Kept filterbar component in wcheader

--- src/Pages/WellCompletion/WCHeader.js
+++ src/Pages/WellCompletion/WCHeader.js
@@ -1,90 +1,95 @@
 import {
-  BusyIndicator,
-  Button,
-  Form,
-  FormItem,
+  FilterBar,
+  FilterGroupItem,
   Icon,
   Input,
 } from "@ui5/webcomponents-react";
-import { useState } from "react";
 
 export const WCHeader = (props) => {
-  const [active, setActive] = useState(false);
-
-  const handleSearchClick = () => {
-    setActive(true);
-  };
-
   return (
-    <Form
-      style={{
-        alignItems: "flex-end",
-        display: "flex",
-        flexWrap: "wrap",
-      }}
+    <FilterBar
+      style={{ maxHeight: "500px" }}
+      showGoOnFB
+      filterContainerWidth="13.125rem"
+      search={<Input placeholder="Search" />}
+      showFilterConfiguration
+      hideToggleFiltersButton //(Hides "Hide Filter Bar" button) This is not working, don't know why
     >
-      <div>
-        <FormItem label="Layout">
-          <BusyIndicator
-            active={active}
-            className=""
-            slot=""
-            style={{}}
-            tooltip=""
-          >
-            <Input icon={<Icon name="search" onClick={handleSearchClick} />} />
-          </BusyIndicator>
-        </FormItem>
-      </div>
-      <div>
-        <FormItem label="Well Number">
-          <Input
-            icon={
-              <Icon
-                name="value-help"
-                onClick={(event) => props.handleValueHelp("well")}
-              />
-            }
-          />
-        </FormItem>
-      </div>
-      <div>
-        <FormItem label="Well Completion">
-          <Input icon={<Icon name="value-help" />} />
-        </FormItem>
-      </div>
-      <div>
-        <FormItem label="WC Name">
-          <Input icon={<Icon name="value-help" />} />
-        </FormItem>
-      </div>
-      <div>
-        <FormItem label="WC Name">
-          <Input icon={<Icon name="value-help" />} />
-        </FormItem>
-      </div>
-      <div>
-        <FormItem label="WC Name">
-          <Input icon={<Icon name="value-help" />} />
-        </FormItem>
-      </div>
-      <div>
-        <FormItem label="WC Name">
-          <Input icon={<Icon name="value-help" />} />
-        </FormItem>
-      </div>
-      <div>
-        <FormItem label="Selection Date">
-          <Input icon={<Icon name="appointment" />} />
-        </FormItem>
-      </div>
-      <div
-        style={{ marginTop: "17px", display: "flex", alignItems: "flex-end" }}
-      >
-        <Button key="go" design="Emphasized">
-          Go
-        </Button>
-      </div>
-    </Form>
+      <FilterGroupItem label="Well Number">
+        <Input
+          icon={<Icon name="value-help" onClick={props.handleValueHelp} />}
+          placeholder=""
+        />
+      </FilterGroupItem>
+      <FilterGroupItem label="Well Completion">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="WC Name">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="API Well Number">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="API WC Number">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="BHL Latitude">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="BHL Longitude">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="Company">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="Cost Center">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="County">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="DOI">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="Major Product">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="Operated">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="Operator">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="Primary DOI">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="Property">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="Rep Well Count">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="SHL Latitude">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="SHL Longitude">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="State">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="WC Type">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="WC Xref 1">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="WC Xref 2">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+      <FilterGroupItem label="Well Class">
+        <Input icon={<Icon name="value-help" />} placeholder="" />
+      </FilterGroupItem>
+    </FilterBar>
   );
 };
